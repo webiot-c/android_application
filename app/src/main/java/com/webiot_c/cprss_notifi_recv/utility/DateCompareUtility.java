5ac84b7bb4477c11d0c9b1ab1ca454826101fa3a
loxygenK <koko0902@outlook.jp>
2019-09-07 20:15:26 +0900

    JavaDocを忘れていた。加えて、日時の差を絶対値で出力するのをやめた。

--- app/src/main/java/com/webiot_c/cprss_notifi_recv/utility/DateCompareUtility.java
+++ app/src/main/java/com/webiot_c/cprss_notifi_recv/utility/DateCompareUtility.java
@@ -6,11 +6,17 @@ import java.util.Date;
 
 public class DateCompareUtility {
 
+    /**
+     * 2つの[ateインスタンスが表す日時の差をms単位で返す。
+     * @param a 比較対象のDate
+     * @param b 比較対象のDate
+     * @return 日時の差(ms単位)。
+     */
     public static long Diff(Date a, Date b){
         long long_aedDate = a.getTime();
         long long_testDate = b.getTime();
 
-        long dayDiff = Math.abs(long_aedDate - long_testDate);
+        long dayDiff = long_aedDate - long_testDate;
 
         Log.e("AED Duration", "[A]: " + a.toString());
         Log.e("AED Duration", "[B]: " + b.toString());
